f617f071fbf36cf177f18e983f9ebbdbdae4a582
Tamago <anamikarai0020806@gmail.com>
2024-07-03 13:16:02 +0545

    updated

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-// console.log("hello world");
-//create local
-const http = require("http");
-const os = require("os");
-const fs = require("fs");
-// http
-//   .createServer((req, res) => {
-//     res.writeHead(200, { "Content-Type": "application/json" });
-//     //calculate os ram
-//     const myRam = os.totalmem().os.fixed(2);
-//     //
-//     res.end(
-//       JSON.stringify({
-//         data: "Hello World!",
-//         data: myRam,
-//       })
-//     );
-//   })
-//   .listen(3000);
-//use a fs module to read the content of file
-//make hello .txt with content
-//use fs
-const data = "Hello,world!";
-fs.writeFile("myile.txt", data, () => {});
+// // console.log("hello world");
+// //create local
+// const http = require("http");
+// const os = require("os");
+// const fs = require("fs");
+// // http
+// //   .createServer((req, res) => {
+// //     res.writeHead(200, { "Content-Type": "application/json" });
+// //     //calculate os ram
+// //     const myRam = os.totalmem().os.fixed(2);
+// //     //
+// //     res.end(
+// //       JSON.stringify({
+// //         data: "Hello World!",
+// //         data: myRam,
+// //       })
+// //     );
+// //   })
+// //   .listen(3000);
+// //use a fs module to read the content of file
+// //make hello .txt with content
+// //use fs
+// const data = "Hello,world!";
+// fs.writeFile("myile.txt", data, () => {});
 
-fs.readFile("myile.txt", "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+// fs.readFile("myile.txt", "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
 
-fs.writeFile("hello.txt", "hello", () => {});
-fs.writefile("hello2.txt", "helloo again", () => {});
-const fs = require("fs");
-const createdoc = (data, path) => {
-  return fs.writefilesync(path, data);
-};
-createdoc("hello", "./hello.txt");
+// fs.writeFile("hello.txt", "hello", () => {});
+// fs.writefile("hello2.txt", "helloo again", () => {});
+// const fs = require("fs");
+// const createdoc = (data, path) => {
+//   return fs.writefilesync(path, data);
+// };
+// createdoc("hello", "./hello.txt");
 
-//require
-//module.exports
+// //require
+// //module.exports
+// const textmanipulater=require("./strng");
+// const {properCase,slugify,truncate};
